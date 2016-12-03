9d0b612bf63959793238a4fbd9b71127805090f2
h22min <ccimin@naver.com>
2016-12-03 19:12:18 +0900

    .

--- src/main/java/kr/ac/hansung/controller/HomeController.java
+++ src/main/java/kr/ac/hansung/controller/HomeController.java
@@ -1,10 +1,8 @@
 package kr.ac.hansung.controller;
 
 
-import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
